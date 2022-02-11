6f4c67105c529cff9685288adeb8a231b9a370a3
graffony <99467948+graffony@users.noreply.github.com>
2022-02-11 10:22:12 +0300

    Add files via upload

---
+++ Weather/Weather/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public class Info
+    {
+        public double temp { get; set; }
+        public int humidity { get; set; }
+        public long sunrise { get; set; }
+        public long sunset { get; set; }
+    }
+}
